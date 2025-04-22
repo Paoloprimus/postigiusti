3d4cf424b98f107966943280d80f494378f47612
Paoloprimus <paolo.olivato@gmail.com>
2025-04-22 09:36:29 +0200

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -61,7 +61,7 @@ export default function Dashboard() {
   return (
     <div className="max-w-2xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">
-        Bacheca Postigiusti
+        Annunci
       </h1>
 
       <div className="space-y-4 mb-8">
