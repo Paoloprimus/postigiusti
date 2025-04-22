134dfecd35dced6e34ac94f995b89a720c1e303e
Paoloprimus <paolo.olivato@gmail.com>
2025-04-22 16:33:04 +0200

    Update announcements.tsx

--- pages/announcements.tsx
+++ pages/announcements.tsx
@@ -1,6 +1,26 @@
 // pages/announcements.tsx
+import { GetServerSideProps } from 'next';
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import Layout from '../components/Layout';
 import AnnouncementsTree from '../components/AnnouncementsTree';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
 
 export default function AnnouncementsPage() {
   return (
