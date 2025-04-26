02698b44ac462dec6515a64758c89a54af34b0de
Paoloprimus <paolo.olivato@gmail.com>
2025-04-26 22:58:09 +0200

    Update comments.ts

--- pages/api/posts/[postId]/comments.ts
+++ pages/api/posts/[postId]/comments.ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         `)
        // .eq('post_id', postId)
         .eq('post_id', postIdNumber)
-        .order('created_at', ascending: false);
+        .order('created_at', { ascending: false });
 
       if (error) return res.status(500).json({ error: error.message });
       return res.status(200).json(data);
