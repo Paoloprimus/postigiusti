ac697e8255e7cac3838d5eb7bdad5f0b5ece8a64
Paoloprimus <paolo.olivato@gmail.com>
2025-04-28 22:43:32 +0200

    Update [postId].ts

--- pages/api/posts/close/[postId].ts
+++ pages/api/posts/close/[postId].ts
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     global: {
       headers: {
-        Authorization: Bearer ${token},
+        Authorization: 'Bearer ${token}',
       },
     },
   });
