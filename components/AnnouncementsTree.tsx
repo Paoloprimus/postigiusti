2fecdafdf02917e8e2924617e6a9b1275814c676
Paoloprimus <paolo.olivato@gmail.com>
2025-04-28 14:59:58 +0200

    Update AnnouncementsTree.tsx

--- components/AnnouncementsTree.tsx
+++ components/AnnouncementsTree.tsx
@@ -331,6 +331,7 @@ function PostList({ provinceId }: { provinceId: number }) {
         </li>
       )}
       {posts.map((post) => (
+      {console.log('Post:', post, 'UserId:', userId)}
         <li key={post.id}>
           <div className={`${getColor(post.type)} cursor-pointer`} onClick={() => handleClick(post.id)}>
             {post.type === 'offro' ? 'OFFRO: ' : 'CERCO: '}
